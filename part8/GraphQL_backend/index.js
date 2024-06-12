702500598a47c7d85ed9a24e9741a30bc2fb7d7b
Elijah-05 <ellayab804@gmail.com>
2024-06-12 19:27:10 +0300

    part 8 exercise 8.14 part-2 solved

--- part8/GraphQL_backend/index.js
+++ part8/GraphQL_backend/index.js
@@ -1,106 +1,114 @@
 const { ApolloServer } = require("@apollo/server");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const { v1: uuid } = require("uuid");
+const Author = require("./models/author");
+const Book = require("./models/book");
+require("dotenv").config();
 
-let authors = [
-  {
-    name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
-  },
-  {
-    name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
-  },
-  {
-    name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-];
+// let authors = [
+//   {
+//     name: "Robert Martin",
+//     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+//     born: 1952,
+//   },
+//   {
+//     name: "Martin Fowler",
+//     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+//     born: 1963,
+//   },
+//   {
+//     name: "Fyodor Dostoevsky",
+//     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+//     born: 1821,
+//   },
+//   {
+//     name: "Joshua Kerievsky", // birthyear not known
+//     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+//   },
+//   {
+//     name: "Sandi Metz", // birthyear not known
+//     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+//   },
+// ];
 
 /*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
  * English:
  * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
  * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
  */
 
-let books = [
-  {
-    title: "Clean Code",
-    published: 2008,
-    author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Agile software development",
-    published: 2002,
-    author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ["agile", "patterns", "design"],
-  },
-  {
-    title: "Refactoring, edition 2",
-    published: 2018,
-    author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Refactoring to patterns",
-    published: 2008,
-    author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "patterns"],
-  },
-  {
-    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-    published: 2012,
-    author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "design"],
-  },
-  {
-    title: "Crime and punishment",
-    published: 1866,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "crime"],
-  },
-  {
-    title: "Demons",
-    published: 1872,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "revolution"],
-  },
-];
+// let books = [
+//   {
+//     title: "Clean Code",
+//     published: 2008,
+//     author: "Robert Martin",
+//     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+//     genres: ["refactoring"],
+//   },
+//   {
+//     title: "Agile software development",
+//     published: 2002,
+//     author: "Robert Martin",
+//     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+//     genres: ["agile", "patterns", "design"],
+//   },
+//   {
+//     title: "Refactoring, edition 2",
+//     published: 2018,
+//     author: "Martin Fowler",
+//     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+//     genres: ["refactoring"],
+//   },
+//   {
+//     title: "Refactoring to patterns",
+//     published: 2008,
+//     author: "Joshua Kerievsky",
+//     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+//     genres: ["refactoring", "patterns"],
+//   },
+//   {
+//     title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+//     published: 2012,
+//     author: "Sandi Metz",
+//     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+//     genres: ["refactoring", "design"],
+//   },
+//   {
+//     title: "Crime and punishment",
+//     published: 1866,
+//     author: "Fyodor Dostoevsky",
+//     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+//     genres: ["classic", "crime"],
+//   },
+//   {
+//     title: "Demons",
+//     published: 1872,
+//     author: "Fyodor Dostoevsky",
+//     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+//     genres: ["classic", "revolution"],
+//   },
+// ];
+
+const MONGODB_URI = process.env.MONGODB_URI;
+console.log("connecting to... ", MONGODB_URI);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connection to MongoDB:", error.message);
+  });
 
 const typeDefs = `
   type Book {
-    title: String
+    title: String!
     published: Int
-    author: String
+    author: Author
+    genres: [String!]!
     id: ID!
-    genres: [String]
   }
   
   type Author {
@@ -113,66 +121,77 @@ const typeDefs = `
   type Query {
     bookCount: Int
     authorCount: Int
-    allBooks(name: String, genre: String): [Book!]!
+    allBooks(title: String, genre: String): [Book!]!
     allAuthors: [Author]
   }
 
   type Mutation {
-    addBook(title: String! author: String! published: Int! genres: [String!]!): Book
+    addBook(title: String!, author: String!, published: Int!, genres: [String!]!): Book
     editAuthor(name: String! setBornTo: Int!): Author
   }
 `;
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
-      if (!args.name && !args.genre) {
-        return books;
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      if (!args.title && !args.genre) {
+        return await Book.find({}).populate("author");
       }
 
-      const filter_author = (book) =>
-        args.name &&
-        book.author.toLowerCase().trim() == args.name.toLowerCase().trim();
-      const filter_genre = (book) =>
-        args.genre ? book.genres.includes(args.genre) : false;
+      return await Book.find({ genres: { $regex: args.genre } });
 
-      const filteredBooks = books.filter((book) => {
-        const filteredAuthor = filter_author(book);
-        const filteredGenre = filter_genre(book);
+      // const filter_author = (book) =>
+      //   args.name &&
+      //   book.author.toLowerCase().trim() == args.name.toLowerCase().trim();
+      // const filter_genre = (book) =>
+      //   args.genre ? book.genres.includes(args.genre) : false;
 
-        if (args.name && args.genre) {
-          return filteredAuthor && filteredGenre;
-        } else if (args.name) {
-          return filteredAuthor;
-        } else if (args.genre) {
-          return filteredGenre;
-        }
-      });
-      return filteredBooks;
+      // const filteredBooks = books.filter((book) => {
+      //   const filteredAuthor = filter_author(book);
+      //   const filteredGenre = filter_genre(book);
+
+      //   if (args.name && args.genre) {
+      //     return filteredAuthor && filteredGenre;
+      //   } else if (args.name) {
+      //     return filteredAuthor;
+      //   } else if (args.genre) {
+      //     return filteredGenre;
+      //   }
+      // });
+      // return filteredBooks;
     },
-    allAuthors: () => authors,
+    allAuthors: async () => await Author.find({}),
   },
   Author: {
-    bookCount: (root) =>
-      books.filter((book) => book.author == root.name).length,
+    bookCount: async (root) => {
+      return await Book.find({ author: root._id }).countDocuments();
+    },
   },
 
   Mutation: {
-    addBook: (root, args) => {
-      const newBook = { ...args, id: uuid() };
-      books = books.concat(newBook);
+    addBook: async (root, args) => {
+      const bookData = { ...args, id: uuid() };
+      const authorData = { name: args.author };
+      const findBook = await Book.findOne({ title: args.title });
+      let findAuthor = await Author.findOne({ name: args.author });
 
-      const findAuthor = authors.find(
-        (author) =>
-          author.name.toLowerCase().trim() === args.author.toLowerCase().trim()
-      );
-      if (!findAuthor) {
-        const newAuthor = { name: args.author, id: uuid() };
-        authors = authors.concat(newAuthor);
+      if (findBook) {
+        return null;
       }
-      return newBook;
+
+      if (!findAuthor) {
+        const newAuthor = new Author(authorData);
+        findAuthor = await newAuthor.save();
+      }
+
+      bookData.author = findAuthor._id;
+      const newBook = new Book(bookData);
+      const saveBookRes = await newBook.save();
+      return await Book.findOne({ title: saveBookRes.title }).populate(
+        "author"
+      );
     },
     editAuthor: (root, args) => {
       const findAuthor = authors.find(
