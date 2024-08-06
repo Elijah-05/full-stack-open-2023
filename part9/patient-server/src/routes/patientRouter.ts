5fe22b380c799587188476503275650b85bb4fb3
Ella <elyas@ablazelabs.io>
2024-08-06 15:57:44 +0300

    patient-server exercise completed and vite exercise initalized

--- part9/patient-server/src/routes/patientRouter.ts
+++ part9/patient-server/src/routes/patientRouter.ts
@@ -11,9 +11,7 @@ router.post("/", (req, res) => {
   console.log({ req });
   try {
     const newPatientEntry = toNewPatientEntry(req.body);
-    console.log("newPatientEntry from Utils: ", newPatientEntry);
     const { ssn, ...addedPatient } = addPatient(newPatientEntry);
-    console.log("addedPatient from Service: ", addedPatient);
     res.json(addedPatient);
   } catch (error: unknown) {
     let errorMessage = "Something went wrong";
