ad00664f5a348515cf485980e9d51cd31925ead8
Ella <elyas@ablazelabs.io>
2024-08-07 19:18:22 +0300

    part9: flight diary frontend exercise completed and the backend cors fix and backend flight-diary git file deleted

--- part9/flight-diary-frontend/src/components/DiaryEntry.tsx
+++ part9/flight-diary-frontend/src/components/DiaryEntry.tsx
@@ -8,9 +8,9 @@ const DiaryEntry = ({ allDiary }: { allDiary: DiaryType[] }) => {
         const { date, visibility, weather } = diary;
         return (
           <div key={diary.id}>
-            <h2 style={{margin: '14px 0 0px 0'}}>{date}</h2>
-            <p style={{margin: 0}}>Visibility: {visibility}</p>
-            <p style={{margin: 0}}>Weather: {weather}</p>
+            <h2 style={{ margin: "14px 0 0px 0" }}>{date}</h2>
+            <p style={{ margin: 0 }}>Visibility: {visibility}</p>
+            <p style={{ margin: 0 }}>Weather: {weather}</p>
           </div>
         );
       })}
